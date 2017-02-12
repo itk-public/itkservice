8b974a9069c71543b1f95a819048ff94126f4215
sno <15051447570@163.com>
2017-02-12 12:33:57 +0800

    project  first commit

---
+++ itk-api/src/main/java/com/itk/user/web/UserController.java
@@ -0,0 +1,31 @@
+package com.itk.user.web;
+
+import com.itk.service.user.api.UserService;
+import com.itk.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public int add() {
+        User user = new User();
+        user.setName("中文");
+        return  userService.insert(user);
+    }
+
+
+    @RequestMapping(value = "/selectById", method = RequestMethod.GET)
+    public User selectById(@RequestParam("id")int id) {
+        return  userService.selectUser(id);
+    }
+
+
+}
