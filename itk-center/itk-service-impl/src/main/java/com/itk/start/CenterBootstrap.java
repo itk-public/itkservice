afefebd244f83a4ad29260ed8eec0123078edcda
snow <15051447570@163.com>
2017-03-06 15:01:30 +0800

    代码规范修改提交

--- itk-center/itk-service-impl/src/main/java/com/itk/start/CenterBootstrap.java
+++ itk-center/itk-service-impl/src/main/java/com/itk/start/CenterBootstrap.java
@@ -37,9 +37,9 @@ public class CenterBootstrap {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource());
 
-        //PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-        //sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*/*.xml"));
+//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//
+//        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*/*.xml"));
 
         return sqlSessionFactoryBean.getObject();
     }
